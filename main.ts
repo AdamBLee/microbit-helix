1519eba744595071f8d93cf6746ed7193b84eabe
Adam Lee <AdamBLee@users.noreply.github.com>
2021-04-23 12:44:46 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+function Helix (brightness: number) {
+    turtle.setBrightness(brightness)
+    turtle.home()
+    turtle.pen(TurtlePenMode.Down)
+    for (let n = 0; n <= 4; n++) {
+        for (let index = 0; index < 2; index++) {
+            turtle.forward(n + 1)
+            turtle.turnRight()
+        }
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    Helix(brightness_high)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        Helix(brightness_high)
+        Helix(brightness_low)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Helix(brightness_low)
+})
+let brightness_low = 0
+let brightness_high = 0
+brightness_high = 128
+brightness_low = 0
